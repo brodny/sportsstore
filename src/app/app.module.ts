f5d11642a79d1e2a15c36246aea5bb136436b736
unknown <brodny@wp.pl>
2020-07-27 23:41:37 +0200

    StoreComponent displayed in the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { StoreModule } from './store/store.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, StoreModule],
   providers: [{ provide: LOCALE_ID, useValue: 'pl-PL', }],
   bootstrap: [AppComponent]
 })
