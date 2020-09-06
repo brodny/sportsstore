1f1d1e18cd2787009acbd47aafab21d19a612be2
unknown <brodny@wp.pl>
2020-09-07 00:00:18 +0200

    tslint warnings fixed

--- src/app/store/counter.directive.ts
+++ src/app/store/counter.directive.ts
@@ -1,18 +1,20 @@
 import { Directive, ViewContainerRef, TemplateRef, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
+  // tslint:disable-next-line: directive-selector
   selector: '[counterOf]'
 })
 export class CounterDirective implements OnChanges {
 
   constructor(private container: ViewContainerRef,
-              private template: TemplateRef<Object>) { }
-  
+              private template: TemplateRef<any>) { }
+
   @Input('counterOf')
   public counter: number;
 
   ngOnChanges(changes: SimpleChanges): void {
     this.container.clear();
+    // tslint:disable-next-line: no-inferrable-types
     for (let i: number = 0; i < this.counter; ++i) {
       this.container.createEmbeddedView(this.template, new CounterDirectiveContext(i + 1));
     }
